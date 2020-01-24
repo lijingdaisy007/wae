6783973590f5f0594f4f7111dc65b533e32298f7
lijingdaisy007 <33889307+lijingdaisy007@users.noreply.github.com>
2020-01-24 15:32:00 +0100

    jupyter notebook compatibility
    The "argparse" is not compatible with jupyter notebook, yet it will appear an early stop due to ValueError in matplotlib. Tyring to fix it anyway.

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ import improved_wae
 from datahandler import DataHandler
 import utils
 
+'''
 parser = argparse.ArgumentParser()
 parser.add_argument("--exp", default='mnist_small',
                     help='dataset [mnist/celebA/dsprites]')
@@ -41,50 +42,14 @@ parser.add_argument("--checkpoint",
                     help='full path to the checkpoint file without extension')
 
 FLAGS = parser.parse_args()
+'''
 
 def main():
 
-    if FLAGS.exp == 'celebA':
-        opts = configs.config_celebA
-    elif FLAGS.exp == 'celebA_small':
-        opts = configs.config_celebA_small
-    elif FLAGS.exp == 'mnist':
-        opts = configs.config_mnist
-    elif FLAGS.exp == 'mnist_small':
-        opts = configs.config_mnist_small
-    elif FLAGS.exp == 'dsprites':
-        opts = configs.config_dsprites
-    elif FLAGS.exp == 'grassli':
-        opts = configs.config_grassli
-    elif FLAGS.exp == 'grassli_small':
-        opts = configs.config_grassli_small
-    else:
-        assert False, 'Unknown experiment configuration'
+    opts = configs.config_mnist
 
-    opts['mode'] = FLAGS.mode
-    if opts['mode'] == 'test':
-        assert FLAGS.checkpoint is not None, 'Checkpoint must be provided'
-        opts['checkpoint'] = FLAGS.checkpoint
-
-    if FLAGS.zdim is not None:
-        opts['zdim'] = FLAGS.zdim
-    if FLAGS.pz is not None:
-        opts['pz'] = FLAGS.pz
-    if FLAGS.lr is not None:
-        opts['lr'] = FLAGS.lr
-    if FLAGS.w_aef is not None:
-        opts['w_aef'] = FLAGS.w_aef
-    if FLAGS.z_test is not None:
-        opts['z_test'] = FLAGS.z_test
-    if FLAGS.lambda_schedule is not None:
-        opts['lambda_schedule'] = FLAGS.lambda_schedule
-    if FLAGS.work_dir is not None:
-        opts['work_dir'] = FLAGS.work_dir
-    if FLAGS.wae_lambda is not None:
-        opts['lambda'] = FLAGS.wae_lambda
-    if FLAGS.enc_noise is not None:
-        opts['e_noise'] = FLAGS.enc_noise
-
+    opts['mode'] = 'train'
+    
     if opts['verbose']:
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
@@ -118,5 +83,3 @@ def main():
 
         # Do something else
         improved_wae.improved_sampling(opts)
-
-main()
